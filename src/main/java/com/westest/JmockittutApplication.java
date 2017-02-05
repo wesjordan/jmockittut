a466f9004b23538cb6b8f5fbb33a8b4bf0e1fba9
wesjordan <jordan.wesley@gmail.com>
2017-02-05 15:11:51 +0200

    src

---
+++ src/main/java/com/westest/JmockittutApplication.java
@@ -0,0 +1,12 @@
+package com.westest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmockittutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmockittutApplication.class, args);
+	}
+}
